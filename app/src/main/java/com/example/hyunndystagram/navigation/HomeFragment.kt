88e83ea2fa5d8dd3937bdb0cd08917a468936d7d
user <yoosa3004@naver.com>
2020-02-29 05:20:42 +0900

    4. 좋아요 버튼 이벤트 활성화. 좋아요 클릭 시 Firebase에 업데이트 되도록 적용

--- app/src/main/java/com/example/hyunndystagram/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/hyunndystagram/navigation/HomeFragment.kt
@@ -10,14 +10,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.hyunndystagram.R
 import com.example.hyunndystagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
+
+/*
+//--------------------------------------------------------------------------------------------------
+작성자: HYEONJIY
+작성일: 2020.02.29
+클래스명: HomeFragment
+설명: action_home 버튼을 눌렀을 때 사진-글이 뜨는 Fragment
+//--------------------------------------------------------------------------------------------------
+ */
 
 class HomeFragment : Fragment() {
 
     // Firebase DB
     var firestore : FirebaseFirestore? = null
+    var currentUseruid : String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,13 +37,14 @@ class HomeFragment : Fragment() {
     ): View? {
 
         firestore = FirebaseFirestore.getInstance()
+        currentUseruid = FirebaseAuth.getInstance().currentUser?.uid
 
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_home, container, false)
-
         // 어댑터(아이템 뷰를 생성)
         view.home_recyclerView.adapter = DetailViewRecyclerViewAdapter()
         // 레이아웃 매니저
         view.home_recyclerView.layoutManager = LinearLayoutManager(activity)
+
         return view
     }
 
@@ -70,7 +82,7 @@ class HomeFragment : Fragment() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
 
-            return CustomViewHolder(view)
+           return CustomViewHolder(view)
         }
 
         // 생성된 데이터는 이미 뷰 홀더가 생성되었을 경우 뷰 홀더를 재생성하지 않고 그냥 bind된다.
@@ -93,10 +105,53 @@ class HomeFragment : Fragment() {
             // 좋아요 수
             viewholder.home_item_favorite_counter.text = "Likes +" + contentDTOs[position]!!.favoriteCount
 
+            setFavoriteBtn(viewholder, position)
         }
 
         override fun getItemCount(): Int {
             return contentDTOs.size
+        }
+
+        private fun setFavoriteBtn(viewHolder : View, position: Int){
+            // 좋아요 버튼 클릭이벤트
+            viewHolder.home_item_favorite_btn.setOnClickListener {
+                favoriteEvent(position)
+            }
+
+            // 위에서 좋아요 처리가 되었다면
+            if(contentDTOs!![position].favorites.contains(currentUseruid)) {
+                viewHolder.home_item_favorite_btn.setImageResource(R.drawable.ic_favorite)
+            }
+            // 위에서 좋아요 처리가 취소되었다면
+            else {
+                viewHolder.home_item_favorite_btn.setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
+
+        private fun favoriteEvent(position: Int){
+
+            // FirebaseDB의 images폴더에 있는 파일들의 document중 선택된 항목의 uid와 일치하는 document를 가져와!
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+
+            firestore?.runTransaction { transaction ->
+
+                // firestoreDB에서 위에서 생성한 참조에 해당하는 오브젝트를 가져왔습니다.
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                // 만약 내가 선택한 게시물에 이미 나의 좋아요가 들어있다면? -> 좋아요 취소
+                if(contentDTO!!.favorites.containsKey(currentUseruid)){
+                    contentDTO?.favoriteCount =  contentDTO?.favoriteCount - 1
+                    contentDTO?.favorites.remove(currentUseruid)
+                }
+                //만약 내가 선택한 게시물에 이미 나의 좋아요가 들어없다면? -> 좋아요!!
+                else {
+                    contentDTO?.favoriteCount =  contentDTO?.favoriteCount + 1
+                    contentDTO?.favorites[currentUseruid!!] = true
+                }
+
+                // DB의 tsDoc 참조에 contentDTO를 새로 써넣어!
+                transaction.set(tsDoc, contentDTO)
+            }
         }
     }
 }
