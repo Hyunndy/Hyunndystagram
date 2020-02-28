d39667c5010dc7d0909f98acd56839af17661b74
user <yoosa3004@naver.com>
2020-02-29 02:34:40 +0900

    2. 하단 네비게이션 바 생성

--- app/src/main/java/com/example/hyunndystagram/MainActivity.kt
+++ app/src/main/java/com/example/hyunndystagram/MainActivity.kt
@@ -2,11 +2,57 @@ package com.example.hyunndystagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.hyunndystagram.navigation.AlarmFragment
+import com.example.hyunndystagram.navigation.GridFragment
+import com.example.hyunndystagram.navigation.HomeFragment
+import com.example.hyunndystagram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+// @HYEONJIY: BottomNavigationView의 항목들이 눌렸을 때 해당 Fragment로 이동시킬 리스너를 구한다.
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            // 홈 화면.
+            R.id.action_home -> {
+                var homeFragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, homeFragment).commit()
+                return true
+            }
+            // 검색 화면
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            // 갤러리로 부터 사진 가져오기
+            R.id.action_add_photo -> {
+                return true
+            }
+            // 좋아요 알림 화면
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            // 계정 상세 정보 화면
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 리스너 세팅.
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
 }
---
+++ app/src/main/java/com/example/hyunndystagram/navigation/AlarmFragment.kt
@@ -0,0 +1,22 @@
+package com.example.hyunndystagram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hyunndystagram.R
+
+class AlarmFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+
+        return view
+    }
+}
